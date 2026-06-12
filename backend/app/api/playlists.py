190332b952ae06115fed340d3ae79be209e49e22
Ts-Joao <cttjoaovts@gmail.com>
2026-06-12 11:48:17 -0300

    refactor(api): separa endpoints de gerenciamento de músicas em playlists

--- backend/app/api/playlists.py
+++ backend/app/api/playlists.py
@@ -5,11 +5,7 @@ from sqlalchemy.orm import Session
 
 from app.database.dependencies import get_db
 from app.repositories.playlist_repository import PlaylistRepository
-from app.repositories.playlist_track_repository import PlaylistTrackRepository
-from app.repositories.track_repository import TrackRepository
-from app.schemas.track import TrackResponseSchema
 from app.services.playlist_service import PlaylistService
-from app.services.playlist_track_service import PlaylistTrackService
 from app.schemas.playlist import (
     CreatePlaylistSchema,
     UpdatePlaylistSchema,
@@ -31,28 +27,6 @@ def create_playlist(
     service = PlaylistService(repository)
 
     return service.create(data)
-
-@router.post(
-    "/{playlist_id}/tracks/{track_id}"
-)
-def add_track(
-        playlist_id: UUID,
-        track_id: UUID,
-        db: Session = Depends(get_db)
-):
-    playlist_repository = PlaylistRepository(db)
-    track_repository = TrackRepository(db)
-    repository = PlaylistTrackRepository(db)
-    service = PlaylistTrackService(
-        repository,
-        playlist_repository,
-        track_repository,
-    )
-
-    return service.add_track(
-        playlist_id,
-        track_id,
-    )
 
 @router.get(
     "/",
@@ -77,25 +51,6 @@ def find_by_id(
 
     return service.find_by_id(playlist_id)
 
-@router.get(
-    "/{playlist_id}/tracks",
-    response_model=list[TrackResponseSchema]
-)
-def find_tracks(
-        playlist_id: UUID,
-        db: Session = Depends(get_db)
-):
-    playlist_repository = PlaylistRepository(db)
-    track_repository = TrackRepository(db)
-    repository = PlaylistTrackRepository(db)
-    service = PlaylistTrackService(
-        repository,
-        playlist_repository,
-        track_repository,
-    )
-
-    return service.find_tracks(playlist_id)
-
 @router.patch(
     "/{playlist_id}",
     response_model=PlaylistResponseSchema
@@ -109,26 +64,6 @@ def update(
     service = PlaylistService(repository)
 
     return  service.update(playlist_id, data)
-
-@router.delete(
-    "/{playlist_id}/tracks/{track_id}",
-    response_model=PlaylistResponseSchema
-)
-def remove_track(
-        playlist_id: UUID,
-        track_id: UUID,
-        db: Session = Depends(get_db)
-):
-    playlist_repository = PlaylistRepository(db)
-    track_repository = TrackRepository(db)
-    repository = PlaylistTrackRepository(db)
-    service = PlaylistTrackService(
-        repository,
-        playlist_repository,
-        track_repository,
-    )
-
-    return service.remove_track(playlist_id, track_id)
 
 @router.delete(
     "/{playlist_id}",
---
+++ backend/app/api/playlists_tracks.py
@@ -0,0 +1,76 @@
+from uuid import UUID
+
+from fastapi import Depends, APIRouter
+from requests import Session
+
+from app.database.dependencies import get_db
+from app.repositories.playlist_repository import PlaylistRepository
+from app.repositories.playlist_track_repository import PlaylistTrackRepository
+from app.repositories.track_repository import TrackRepository
+from app.schemas.playlist import PlaylistResponseSchema
+from app.schemas.track import TrackResponseSchema
+from app.services.playlist_track_service import PlaylistTrackService
+
+
+router = APIRouter(prefix="/playlist", tags=["Playlist Tracks"])
+
+@router.post(
+    "/{playlist_id}/tracks/{track_id}"
+)
+def add_track(
+        playlist_id: UUID,
+        track_id: UUID,
+        db: Session = Depends(get_db)
+):
+    playlist_repository = PlaylistRepository(db)
+    track_repository = TrackRepository(db)
+    repository = PlaylistTrackRepository(db)
+    service = PlaylistTrackService(
+        repository,
+        playlist_repository,
+        track_repository,
+    )
+
+    return service.add_track(
+        playlist_id,
+        track_id,
+    )
+
+@router.get(
+    "/{playlist_id}/tracks",
+    response_model=list[TrackResponseSchema]
+)
+def find_tracks(
+        playlist_id: UUID,
+        db: Session = Depends(get_db)
+):
+    playlist_repository = PlaylistRepository(db)
+    track_repository = TrackRepository(db)
+    repository = PlaylistTrackRepository(db)
+    service = PlaylistTrackService(
+        repository,
+        playlist_repository,
+        track_repository,
+    )
+
+    return service.find_tracks(playlist_id)
+
+@router.delete(
+    "/{playlist_id}/tracks/{track_id}",
+    response_model=PlaylistResponseSchema
+)
+def remove_track(
+        playlist_id: UUID,
+        track_id: UUID,
+        db: Session = Depends(get_db)
+):
+    playlist_repository = PlaylistRepository(db)
+    track_repository = TrackRepository(db)
+    repository = PlaylistTrackRepository(db)
+    service = PlaylistTrackService(
+        repository,
+        playlist_repository,
+        track_repository,
+    )
+
+    return service.remove_track(playlist_id, track_id)
