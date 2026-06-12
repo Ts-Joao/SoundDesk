53060dd5ef2ef18d130d30da7166a388714e1934
Ts-Joao <cttjoaovts@gmail.com>
2026-06-12 10:58:11 -0300

    setup: configura setup do redis e worker

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,6 +4,7 @@ from fastapi.responses import JSONResponse
 from app.core.exceptions import AppException
 from app.api.playlists import router as playlist_router
 from app.api.tracks import router as track_router
+from app.workers.tasks import test_task
 
 
 app = FastAPI(title="SoundDesk API")
@@ -19,6 +20,14 @@ async def app_exception_handler(
             "message": exc.message
         },
     )
+
+@app.get("/test")
+def test():
+    test_task.delay()
+
+    return {
+       "message": "Task sent successfully"
+    }
 
 app.include_router(playlist_router)
 app.include_router(track_router)
