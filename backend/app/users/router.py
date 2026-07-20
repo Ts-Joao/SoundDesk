2ebe82a713da3018947ad8aa71f4099a2cad29d0
Ts-Joao <cttjoaovts@gmail.com>
2026-07-20 20:29:00 -0300

    feat: implementa endpoints restantes de usuario

--- backend/app/users/router.py
+++ backend/app/users/router.py
@@ -4,10 +4,12 @@ from uuid import UUID
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
+from app.auth.dependencies import get_current_user
 from app.database.dependencies import get_db
 from app.users.repository import UserRepository
-from app.users.schemas import UserResponseSchema, CreateUserSchema
+from app.users.schemas import UserResponseSchema, UpdateUserSchema
 from app.users.service import UserService
+from app.users.models import User
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
@@ -19,6 +21,43 @@ def get_all(db: Session = Depends(get_db)):
     repository = UserRepository(db)
     service = UserService(repository)
     return service.find_all()
+
+@router.get(
+    "/me",
+    response_model=UserResponseSchema,
+)
+def get_me(
+        current_user: User = Depends(get_current_user),
+        db: Session = Depends(get_db),
+):
+    repository = UserRepository(db)
+    service = UserService(repository)
+    return service.find_by_id(current_user.id)
+
+@router.patch(
+    "/me",
+    response_model=UserResponseSchema,
+)
+def update(
+        data: UpdateUserSchema,
+        db: Session = Depends(get_db),
+        current_user: User = Depends(get_current_user)
+):
+    repository = UserRepository(db)
+    service = UserService(repository)
+    return service.update(current_user.id, data)
+
+@router.delete(
+    "/me",
+    status_code=200,
+)
+def delete_me(
+        current_user: User = Depends(get_current_user),
+        db: Session = Depends(get_db),
+):
+    repository = UserRepository(db)
+    service = UserService(repository)
+    return service.delete_user(current_user.id)
 
 @router.delete(
     "/{user_id}",
